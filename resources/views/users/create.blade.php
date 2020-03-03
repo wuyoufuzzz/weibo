3eef8aa190957a843ecdaf9977fba7b15008e8e4
wuyoufuzzz <zhucelehaojiu@gmail.com>
2020-03-03 10:36:30 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,19 +2,15 @@
 @section('title', '注册')
 
 @section('content')
-   
-
-   
-
-
-
-<div class="offset-md-2 col-md-8">
+    <div class="offset-md-2 col-md-8">
         <div class="card">
             <div class="card-header">
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+                @include('shared._error')
                 <form method="POST" action="{{ route('users.store') }}">
+                {{ csrf_field() }}
                     <div class="form-group">
                         <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
