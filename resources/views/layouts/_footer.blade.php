1c14591b0ae6c71f6deadd9ba68c767bc5b96a27
wuyoufuzzz <zhucelehaojiu@gmail.com>
2020-03-03 07:37:15 +0000

    切割头部和尾部子视图---更正上次记录，静态文件浏览器缓存问题

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class='footer'>
+    <img class='brant-icon' src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
+    <a href="http://weibo.test" target='_blank'>刻意练习，每日精进</a>
+
+    <div class='float-right'>
+        <a href="/about">关于</a>
+    </div>
+</footer>
