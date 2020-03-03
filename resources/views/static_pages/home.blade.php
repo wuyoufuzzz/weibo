da2826b84bfb2c807b94b1bbb260348ff74611a3
wuyoufuzzz <zhucelehaojiu@gmail.com>
2020-03-03 07:15:18 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,12 @@
 @section('title', '主页')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">你现在所看到的是 <a href="http://weibo.test">Laravel 入门教程</a>的示例主页</p>
+        <p>一切，将从这里开始。</p>
+        <p>
+            <a class='btn btn-lg btn-success' href="#" role='button'>现在注册</a>
+        </p>
+    </div>
 @stop
