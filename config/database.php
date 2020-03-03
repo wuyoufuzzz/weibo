c9f0a4a9ad814eb7aed27cbea12d504a3b13b271
wuyoufuzzz <zhucelehaojiu@gmail.com>
2020-03-03 15:56:46 +0000

    修改数据库文件

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $db_config('host'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
             'database' => $db_config['database'],
             'username' => $db_config['username'],
