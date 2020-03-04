3875a670939cd6b81d58864a8373be11e33a6f6f
wuyoufuzzz <zhucelehaojiu@gmail.com>
2020-03-04 15:31:57 +0000

    更新用户资料

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -5,41 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 
-class SessionController extends Controller
-{
+class SessionController extends Controller {
     //登录表单
-    public function create()
-    {
-        return view('sessions.create');
+
+    public function create () {
+        return view( 'sessions.create' );
     }
 
     // 验证用户数据
 
-    public function store(Request $request)
-    {
+    public function store( Request $request ) {
         // 取得数据
-        $credentials = $this->validate($request, [
+        $credentials = $this->validate( $request, [
             'email' => 'required|email|max:255',
             'password' => 'required'
-        ]);
-        // dd($credentials);
+        ] );
+        // dd( $credentials );
         // 验证数据是否存在数据库
-        if (Auth::attempt($credentials)) {
+        if ( Auth::attempt( $credentials, $request->has( 'remember' ) ) ) {
             // 登录成功
-            session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            session()->flash( 'success', '欢迎回来！' );
+            return redirect()->route( 'users.show', [Auth::user()] );
         } else {
             // 登录失败
-            session()->flash('danger', '很抱歉，您的邮箱或者是密码不正确');
+            session()->flash( 'danger', '很抱歉，您的邮箱或者是密码不正确' );
             return redirect()->back()->withInput();
         }
     }
 
     // 用户退出
-    public function destroy()
-    {
-        Auth::logout(); //用户退出
-        session()->flash('success', '您已成功退出!');
-        return redirect('login');
+
+    public function destroy() {
+        Auth::logout();
+        //用户退出
+        session()->flash( 'success', '您已成功退出!' );
+        return redirect( 'login' );
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,39 +6,61 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     //用户操作
-    public function create()
-    {
-        return view('users.create');
+
+    public function create() {
+        return view( 'users.create' );
     }
 
-    public function show(User $user)
-    {
-       
-        return view('users.show', compact('user'));
+    public function show( User $user ) {
+        return view( 'users.show', compact( 'user' ) );
     }
 
-    public function store(Request $request)
-    {
-        $this->validate($request, [
+    public function store( Request $request ) {
+        $this->validate( $request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
-        ]);
-        
-        $user = User::create([
+        ] );
+
+        $user = User::create( [
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+            'password' => bcrypt( $request->password ),
+        ] );
 
         // 自动登录
-        Auth::login($user);
+        Auth::login( $user );
 
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        
-        return redirect()->route('users.show', [$user]);
+        session()->flash( 'success', '欢迎，您将在这里开启一段新的旅程~' );
+
+        return redirect()->route( 'users.show', [$user] );
+    }
+
+    // 用户更新资料入口
+
+    public function edit( User $user ) {
+        return view( 'users.edit', compact( 'user' ) );
+    }
+
+    // 提交更新个人资料入口
+
+    public function update( User $user, Request $request ) {
+        // 获取提交的用户数据
+        $this->validate( $request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ] );
+
+        // 向数据库更新用户的数据
+        $data = [];
+        $data['name'] = $request->name;
+        if ( $request->password ) {
+            $data['password'] = bcrypt( $request->password );
+        }
+        $user->update( $data );
+        session()->flash( 'success', '更新成功！' );
+        return redirect()->route( 'users.show', $user );
     }
 }
